d3a3d22563c94ff15894b8404576687f59538277
ketherball <121590283+ketherball@users.noreply.github.com>
2025-09-06 15:31:50 -0400

    Add modeling logic for invocables

--- core/usecases/Modeler.java
+++ core/usecases/Modeler.java
@@ -1,5 +1,8 @@
 package core.usecases;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 import core.entities.Doc;
 import core.entities.Doc.Text;
 import core.entities.Doc.Wrapper;
@@ -15,6 +18,8 @@ public class Modeler {
             obj instanceof Float   ||
             obj instanceof Double)           return modelNum(obj);
         if (obj instanceof Boolean bool)     return modelBoolean(bool);
+        if (obj instanceof Method m)         return modelMethod(m);
+        if (obj instanceof Constructor c)    return modelConstructor(c);
                                              return new Text(obj.toString());
     }
     private static final Doc modelNull(Object obj) {
@@ -25,5 +30,11 @@ public class Modeler {
     }
     private static final Doc modelBoolean(Boolean bool) {
         return new Wrapper(new Text(bool.toString()), WrapperType.FG_COLOR_YELLOW);
+    }
+    private static final Doc modelMethod(Method m) {
+        return new Wrapper(new Text("[Method: "+m.getName()+"]"), WrapperType.FG_COLOR_CYAN);
+    }
+    private static final Doc modelConstructor(Constructor<?> c) {
+        return new Wrapper(new Text("[Constructor: "+c.getDeclaringClass().getSimpleName()+"]"), WrapperType.FG_COLOR_CYAN);
     }
 }
