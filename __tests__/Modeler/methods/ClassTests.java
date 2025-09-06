f3e0b0e8561be0777957578ae850e554f4bb6805
ketherball <121590283+ketherball@users.noreply.github.com>
2025-09-06 15:49:09 -0400

    Add tests for custom classes to class test suite

--- __tests__/Modeler/methods/ClassTests.java
+++ __tests__/Modeler/methods/ClassTests.java
@@ -4,8 +4,15 @@ import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 
+import __tests__.__utils__.TestClass;
+import __tests__.__utils__.TestClass2;
 import core.entities.Doc;
+import core.entities.Doc.Concat;
+import core.entities.Doc.IndentBlock;
+import core.entities.Doc.LineBreak;
 import core.entities.Doc.Text;
+import core.entities.Doc.Wrapper;
+import core.entities.Doc.WrapperType;
 import core.usecases.Modeler;
 import shared.testing.Test;
 
@@ -30,6 +37,62 @@ public class ClassTests {
             Doc result = Modeler.model(List.class);
 
             Test.expect(result.equals(new Text("[interface List]")));
+        });
+
+        Test.it("Models Class correctly (custom)", () -> {
+            Doc result = Modeler.model(TestClass.class);
+
+            Test.expect(result.equals(
+                new Concat(List.of(
+                    new Text("[class TestClass] "),
+                    new Wrapper(new Concat(List.of(
+                        new LineBreak(),
+                        new IndentBlock(List.of(
+                            new Concat(List.of(
+                                new Text("staticProp"),
+                                new Text(": "),
+                                new Wrapper(new Wrapper(new Text("Static prop"), WrapperType.SINGLE_QUOTE), WrapperType.FG_COLOR_GREEN),
+                                new Text(", "))
+                            ),
+                            new LineBreak(),
+                            new Concat(List.of(
+                                new Text("staticMethod"),
+                                new Text(": "),
+                                new Wrapper(new Text("[Method: staticMethod]"), WrapperType.FG_COLOR_CYAN)
+                            )),
+                            new LineBreak()
+                        ))
+                    )), WrapperType.CURLY_BRACKETS)
+                ))
+            ));
+        });
+
+        Test.it("Models Class correctly (custom 2)", () -> {
+            Doc result = Modeler.model(TestClass2.class);
+
+            Test.expect(result.equals(
+                new Concat(List.of(
+                    new Text("[class TestClass2] "),
+                    new Wrapper(new Concat(List.of(
+                        new LineBreak(),
+                        new IndentBlock(List.of(
+                            new Concat(List.of(
+                                new Text("staticProp2"),
+                                new Text(": "),
+                                new Wrapper(new Wrapper(new Text("Static prop"), WrapperType.SINGLE_QUOTE), WrapperType.FG_COLOR_GREEN),
+                                new Text(", "))
+                            ),
+                            new LineBreak(),
+                            new Concat(List.of(
+                                new Text("staticMethod2"),
+                                new Text(": "),
+                                new Wrapper(new Text("[Method: staticMethod2]"), WrapperType.FG_COLOR_CYAN)
+                            )),
+                            new LineBreak()
+                        ))
+                    )), WrapperType.CURLY_BRACKETS)
+                ))
+            ));
         });
     }
 }
