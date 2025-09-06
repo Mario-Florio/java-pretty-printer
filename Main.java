1d1dcb3a0a0c0e24dc4e962a609a1936c6b962be
ketherball <121590283+ketherball@users.noreply.github.com>
2025-09-06 17:18:20 -0400

    Minor name changes and updates to Main

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
-import __tests__.Modeler.ModelerTest;
+import __tests__.Modeler.ModelerTests;
+import __tests__.StringRenderer.StringRendererTests;
 
 public class Main {
     public static void main(String[] args) {
         tests();
     }
     private static void tests() {
-        ModelerTest.run();
+        ModelerTests.run();
+        StringRendererTests.run();
     }
 }
