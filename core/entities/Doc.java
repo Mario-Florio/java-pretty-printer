032980a4998c5387fc4430276461d241416e9ab3
ketherball <121590283+ketherball@users.noreply.github.com>
2025-09-06 16:56:44 -0400

    Add rendering logic for wrapper docs
    Implement StringRenderer as visitor to Doc to utilize polymorhpism while maintaining separation of concerns.

--- core/entities/Doc.java
+++ core/entities/Doc.java
@@ -5,22 +5,59 @@ import java.util.List;
 public sealed interface Doc permits
     Doc.Text, Doc.Wrapper, Doc.Concat, Doc.LineBreak, Doc.IndentBlock {
 
+    public String render(Renderer renderer, int indentLevel);
+
     enum WrapperType {
         SINGLE_QUOTE, SQUARE_BRACKETS, CURLY_BRACKETS,
         FG_COLOR_GREEN, FG_COLOR_CYAN, FG_COLOR_YELLOW
     }
 
-    public record Text(String value) implements Doc {}
+    interface Renderer {
+        default String render(Doc docTree) { return docTree.render(this, 0); };
+        public String render(Doc docTree, int indentLevel);
+        public String renderText(Text text, int indentLevel);
+        public String renderWrapper(Wrapper wrapper, int indentLevel);
+        public String renderConcat(Concat concat, int indentLevel);
+        public String renderLineBreak(LineBreak lb, int indentLevel);
+        public String renderIndentBlock(IndentBlock ib, int indentLevel);
+    }
 
-    public record Wrapper(Doc doc, WrapperType type) implements Doc {}
+    public record Text(String value) implements Doc {
+        @Override
+        public String render(Renderer renderer, int indentLevel) {
+            return renderer.renderText(this, indentLevel);
+        }
+    }
 
-    public record Concat(List<Doc> children) implements Doc {}
+    public record Wrapper(Doc doc, WrapperType type) implements Doc {
+        @Override
+        public String render(Renderer renderer, int indentLevel) {
+            return renderer.renderWrapper(this, indentLevel);
+        }
+    }
+
+    public record Concat(List<Doc> children) implements Doc {
+        @Override
+        public String render(Renderer renderer, int indentLevel) {
+            return renderer.renderConcat(this, indentLevel);
+        }
+    }
 
     public record LineBreak() implements Doc {
         public static final String value = "\n";
+
+        @Override
+        public String render(Renderer renderer, int indentLevel) {
+            return renderer.renderLineBreak(this, indentLevel);
+        }
     }
 
     public record IndentBlock(List<Doc> children) implements Doc {
         public static String INDENT = "  ";
+
+        @Override
+        public String render(Renderer renderer, int indentLevel) {
+            return renderer.renderIndentBlock(this, indentLevel);
+        }
     }
 }
--- core/usecases/StringRenderer.java
+++ core/usecases/StringRenderer.java
@@ -1,14 +1,91 @@
 package core.usecases;
 
 import core.entities.Doc;
+import core.entities.Doc.Concat;
+import core.entities.Doc.IndentBlock;
+import core.entities.Doc.LineBreak;
+import core.entities.Doc.Renderer;
 import core.entities.Doc.Text;
+import core.entities.Doc.Wrapper;
+import core.entities.Doc.WrapperType;
 
-public class StringRenderer {
-    public String render(Doc doc) {
-        return renderText((Text) doc);
+public class StringRenderer implements Renderer {
+
+    @Override
+    public String render(Doc doc, int indentLevel) {
+        return doc.render(this, indentLevel);
     }
 
-    public String renderText(Text doc) {
-        return doc.value();
+    @Override
+    public String renderText(Text text, int indentLevel) {
+        return text.value();
+    }
+
+    @Override
+    public String renderWrapper(Wrapper wrapper, int indentLevel) {
+        switch (wrapper.type()) {
+
+            case WrapperType.SINGLE_QUOTE:
+                return singleQuoteWrapper(wrapper.doc().render(this, indentLevel));
+
+            case WrapperType.SQUARE_BRACKETS:
+                return squareBracketWrapper(wrapper.doc().render(this, indentLevel));
+
+            case WrapperType.CURLY_BRACKETS:
+                return curlyBracketWrapper(wrapper.doc().render(this, indentLevel));
+
+            case WrapperType.FG_COLOR_GREEN:
+                return fgGreenWrapper(wrapper.doc().render(this, indentLevel));
+
+            case WrapperType.FG_COLOR_YELLOW:
+                return fgYellowWrapper(wrapper.doc().render(this, indentLevel));
+
+            case WrapperType.FG_COLOR_CYAN:
+                return fgCyanWrapper(wrapper.doc().render(this, indentLevel));
+
+            default:
+                return wrapper.doc().render(this, indentLevel);
+
+        }
+    }
+
+    @Override
+    public String renderConcat(Concat concat, int indentLevel) {
+        return "";
+    }
+
+    @Override
+    public String renderLineBreak(LineBreak lb, int indentLevel) {
+        return "";
+    }
+
+    @Override
+    public String renderIndentBlock(IndentBlock ib, int indentLevel) {
+        return "";
+    }
+
+    // UTILS
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String RESET = "\u001B[0m";
+
+    private static final String singleQuoteWrapper(String str) {
+        return "'"+str+"'";
+    }
+    private static final String squareBracketWrapper(String str) {
+        return "["+str+"]";
+    }
+    private static final String curlyBracketWrapper(String str) {
+        return "{"+str+"}";
+    }
+    private static final String fgGreenWrapper(String str) {
+        return GREEN+str+RESET;
+    }
+    private static final String fgYellowWrapper(String str) {
+        return YELLOW+str+RESET;
+    }
+    private static final String fgCyanWrapper(String str) {
+        return CYAN+str+RESET;
     }
 }
