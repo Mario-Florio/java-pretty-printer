7463247de4730da3f0a2931a7c471bc20c9a32a0
ketherball <121590283+ketherball@users.noreply.github.com>
2025-09-06 15:35:30 -0400

    Add modeling logic for functions

--- core/usecases/Modeler.java
+++ core/usecases/Modeler.java
@@ -2,6 +2,12 @@ package core.usecases;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import core.entities.Doc;
 import core.entities.Doc.Text;
@@ -20,6 +26,13 @@ public class Modeler {
         if (obj instanceof Boolean bool)     return modelBoolean(bool);
         if (obj instanceof Method m)         return modelMethod(m);
         if (obj instanceof Constructor c)    return modelConstructor(c);
+        if (obj instanceof Runnable r)       return modelRunnable(r);
+        if (obj instanceof Callable c)       return modelCallable(c);
+        if (obj instanceof Function f)       return modelFunc(f);
+        if (obj instanceof Consumer c)       return modelConsumer(c);
+        if (obj instanceof BiFunction bf)    return modelBiFunc(bf);
+        if (obj instanceof BiConsumer bc)    return modelBiConsumer(bc);
+        if (obj instanceof Supplier s)       return modelSupplier(s);
                                              return new Text(obj.toString());
     }
     private static final Doc modelNull(Object obj) {
@@ -36,5 +49,26 @@ public class Modeler {
     }
     private static final Doc modelConstructor(Constructor<?> c) {
         return new Wrapper(new Text("[Constructor: "+c.getDeclaringClass().getSimpleName()+"]"), WrapperType.FG_COLOR_CYAN);
+    }
+    private static final Doc modelRunnable(Runnable r) {
+        return new Wrapper(new Text("[Runnable]"), WrapperType.FG_COLOR_CYAN);
+    }
+    private static final Doc modelCallable(Callable<?> c) {
+        return new Wrapper(new Text("[Callable]"), WrapperType.FG_COLOR_CYAN);
+    }
+    private static final Doc modelFunc(Function<?, ?> f) {
+        return new Wrapper(new Text("[Function]"), WrapperType.FG_COLOR_CYAN);
+    }
+    private static final Doc modelConsumer(Consumer<?> c) {
+        return new Wrapper(new Text("[Consumer]"), WrapperType.FG_COLOR_CYAN);
+    }
+    private static final Doc modelBiFunc(BiFunction<?, ?, ?> bf) {
+        return new Wrapper(new Text("[BiFunction]"), WrapperType.FG_COLOR_CYAN);
+    }
+    private static final Doc modelBiConsumer(BiConsumer<?, ?> bc) {
+        return new Wrapper(new Text("[BiConsumer]"), WrapperType.FG_COLOR_CYAN);
+    }
+    private static final Doc modelSupplier(Supplier<?> s) {
+        return new Wrapper(new Text("[Supplier]"), WrapperType.FG_COLOR_CYAN);
     }
 }
