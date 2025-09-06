9257bb56fe5af4268769f48f498b335a01f58857
ketherball <121590283+ketherball@users.noreply.github.com>
2025-09-06 17:28:24 -0400

    Add Engine orchestration logic

--- interfaceadapters/Engine.java
+++ interfaceadapters/Engine.java
@@ -1,8 +1,16 @@
 package interfaceadapters;
 
+import core.entities.Doc;
+import core.entities.Doc.Renderer;
+import core.usecases.Modeler;
+import core.usecases.StringRenderer;
+
 public class Engine {
+    private Renderer renderer = new StringRenderer();
 
     public String run(Object obj) {
-        return "";
+        Doc docTree = Modeler.model(obj);
+
+        return renderer.render(docTree);
     }
 }
