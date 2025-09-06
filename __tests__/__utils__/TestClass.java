6ba52ebc0cdafe49c135ced6b70a053bd518e26f
ketherball <121590283+ketherball@users.noreply.github.com>
2025-09-06 15:53:44 -0400

    Add test classes for custom class tests
    Classes are for previous commit - forgot to add to commit.

---
+++ __tests__/__utils__/TestClass.java
@@ -0,0 +1,8 @@
+package __tests__.__utils__;
+
+public class TestClass {
+    public static String staticProp = "Static prop";
+    public String prop = "Prop";
+    public static void staticMethod() {}
+    public void method() {}
+}
---
+++ __tests__/__utils__/TestClass2.java
@@ -0,0 +1,8 @@
+package __tests__.__utils__;
+
+public class TestClass2 {
+    public static String staticProp2 = "Static prop";
+    public String prop2 = "Prop";
+    public static void staticMethod2() {}
+    public void method2() {}
+}
