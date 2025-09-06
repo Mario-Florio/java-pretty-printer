d57dab3b982d5412e6ce75bc01cb660b17274730
ketherball <121590283+ketherball@users.noreply.github.com>
2025-09-06 16:27:21 -0400

    Add rendering logic for text docs

--- core/usecases/StringRenderer.java
+++ core/usecases/StringRenderer.java
@@ -1,10 +1,14 @@
 package core.usecases;
 
 import core.entities.Doc;
+import core.entities.Doc.Text;
 
 public class StringRenderer {
-
     public String render(Doc doc) {
-        return "";
+        return renderText((Text) doc);
+    }
+
+    public String renderText(Text doc) {
+        return doc.value();
     }
 }
